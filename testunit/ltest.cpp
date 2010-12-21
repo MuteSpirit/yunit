4a51ee0146935de23afe56123cd5602dcd276969
Efimov Ivan aka MuteSpirit <mutespirit@gmail.com>
2010-12-21 13:01:05 +0500

    w04895 ?????? ??????????? atd ?? filesystem (r09634)

---
+++ testunit/ltest.cpp
@@ -0,0 +1,229 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// test.cpp
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lua\lauxlib.h"
+#include "lua\lualib.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "test.h"
+
+
+TESTUNIT_NS_BEGIN
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Bindings for Lua
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TestCase at Lua code:
+// {
+//		setUp = setUpCFunction,
+//		test = testCFunction,
+//		tearDown = tearDownCFunction,
+//		this = userdata,	// pointer to C++ object of class TestCase
+// }
+
+static Thunk getSetUpThunk(TESTUNIT_NS::TestCase* testCase)
+{
+	return testCase->setUpThunk();
+}
+
+static Thunk getTestThunk(TESTUNIT_NS::TestCase* testCase)
+{
+	return testCase->testThunk();
+}
+
+static Thunk getTearDownThunk(TESTUNIT_NS::TestCase* testCase)
+{
+	return testCase->tearDownThunk();
+}
+
+static const char* getFuncName(Thunk (*getThunkFunc)(TESTUNIT_NS::TestCase*))
+{
+	if (&getSetUpThunk == getThunkFunc)
+		return "setUp";
+	else if (&getTestThunk == getThunkFunc)
+		return "test";
+	else if (&getTearDownThunk == getThunkFunc)
+		return "tearDown";
+
+	return "[unknown]";
+}
+
+static int callTestCaseThunk(lua_State *L, Thunk (*getThunkFunc)(TESTUNIT_NS::TestCase*))
+{
+	lua_getfield(L, 1, "this");
+	TESTUNIT_NS::TestCase* testCase = static_cast<TESTUNIT_NS::TestCase*>(lua_touserdata(L, -1));
+    try
+    {
+        (*getThunkFunc)(testCase).invoke();
+    }
+	catch (TESTUNIT_NS::TestException& ex)
+    {
+		lua_pushboolean(L, false);			// status code
+
+		lua_newtable(L);					// ErrorObject table
+
+		lua_pushstring(L, ex.sourceLine().fileName());
+		lua_setfield(L, -2, "source");		// source file with error
+
+		lua_pushfstring(L, "%s::%s()", testCase->name(), getFuncName(getThunkFunc));
+		lua_setfield(L, -2, "func");		// function with error
+
+		lua_pushinteger(L, ex.sourceLine().lineNumber());
+		lua_setfield(L, -2, "line");		// number of line with error
+
+		enum {bufferSize = 1024 * 10};
+		char buffer[bufferSize] = {'\0'};
+		ex.message(buffer, bufferSize);
+		lua_pushstring(L, buffer);			// error message
+		lua_setfield(L, -2, "message");
+		// now we have bool and table on the top of Lua stack
+
+		return 2;
+    }
+    catch(std::exception& ex)
+    {
+		lua_pushboolean(L, false);			// status code
+
+		lua_newtable(L);					// ErrorObject table
+
+		lua_pushstring(L, "");
+		lua_setfield(L, -2, "source");		// source file with error
+
+		lua_pushfstring(L, "%s::%s()", testCase->name(), getFuncName(getThunkFunc));
+		lua_setfield(L, -2, "func");		// function with error
+
+		lua_pushinteger(L, 0);
+		lua_setfield(L, -2, "line");		// number of line with error
+
+		lua_pushfstring(L, "Unexpected std::exception was caught: %s", ex.what());
+		lua_setfield(L, -2, "message");
+		// now we have bool and table on the top of Lua stack
+
+		return 2;
+	}
+    catch(...)
+    {
+		lua_pushboolean(L, false);			// status code
+
+		lua_newtable(L);					// ErrorObject table
+
+		lua_pushstring(L, "");
+		lua_setfield(L, -2, "source");		// source file with error
+
+		lua_pushfstring(L, "%s::%s()", testCase->name(), getFuncName(getThunkFunc));
+		lua_setfield(L, -2, "func");		// function with error
+
+		lua_pushinteger(L, 0);
+		lua_setfield(L, -2, "line");		// number of line with error
+
+		lua_pushstring(L, "Unexpected unknown exception was caught");
+		lua_setfield(L, -2, "message");
+		// now we have bool and table on the top of Lua stack
+
+		return 2;
+	}
+
+	lua_pushboolean(L, true);			// status code
+
+	lua_newtable(L);					// ErrorObject table
+
+	lua_pushstring(L, "");
+	lua_setfield(L, -2, "source");
+
+	lua_pushfstring(L, "%s::%s()", testCase->name(), getFuncName(getThunkFunc));
+	lua_setfield(L, -2, "func");		// function with error
+
+	lua_pushinteger(L, 0);
+	lua_setfield(L, -2, "line");		// number of line with error
+
+	lua_pushstring(L, "");
+	lua_setfield(L, -2, "message");
+
+	return 2;
+}
+
+int luaTestCaseSetUp(lua_State *L)
+{
+	return callTestCaseThunk(L, getSetUpThunk);
+}
+
+int luaTestCaseTest(lua_State *L)
+{
+	return callTestCaseThunk(L, getTestThunk);
+}
+
+int luaTestCaseTearDown(lua_State *L)
+{
+	return callTestCaseThunk(L, getTearDownThunk);
+}
+
+int getTestList(lua_State *L)
+{
+	lua_newtable(L); // table of all test cases
+	lua_Number i = 1;
+
+	TESTUNIT_NS::TestRegistry::TestSuiteIter it = TESTUNIT_NS::TestRegistry::initialize()->beginTestSuites();
+	TESTUNIT_NS::TestRegistry::TestSuiteIter itEnd = TESTUNIT_NS::TestRegistry::initialize()->endTestSuites();
+	for(; it != itEnd; ++it)
+	{
+		TESTUNIT_NS::TestSuite::TestCaseIter itTc = (*it)->beginTestCases();
+		TESTUNIT_NS::TestSuite::TestCaseIter itTcEnd = (*it)->endTestCases();
+		for(; itTc != itTcEnd; ++itTc)
+		{
+			lua_pushnumber(L, i++);	// order number of TestCase
+
+			lua_newtable(L);	// TestCase
+			// t["this"] = *itTc
+			lua_pushlightuserdata(L, (*itTc));
+			lua_setfield(L, -2, "this");
+			// t["setUp"] = luaTestCaseSetUp
+			lua_pushcfunction(L, TESTUNIT_NS::luaTestCaseSetUp);
+			lua_setfield(L, -2, "setUp");
+			// t["test"] = luaTestCaseTest
+			lua_pushcfunction(L, TESTUNIT_NS::luaTestCaseTest);
+			lua_setfield(L, -2, "test");
+			// t["tearDown"] = luaTestCaseTearDown
+			lua_pushcfunction(L, TESTUNIT_NS::luaTestCaseTearDown);
+			lua_setfield(L, -2, "tearDown");
+			// t["name_"] =
+			lua_pushfstring(L, "%s::%s", (*it)->name(), (*itTc)->name());
+			lua_setfield(L, -2, "name_");
+			// t["isIgnored_"] =
+			lua_pushboolean(L, (*itTc)->isIgnored());
+			lua_setfield(L, -2, "isIgnored_");
+
+			// add table of TestCase into common list
+			// t[i] = testcase
+			lua_settable(L, -3);
+		}
+	}
+
+	return 1;
+}
+
+TESTUNIT_NS_END
+
+
+static const struct luaL_Reg cppunitLuaFunctions[] =
+{
+	{"getTestList", TESTUNIT_NS::getTestList},
+	{NULL, NULL},
+};
+
+extern "C"
+int TESTUNIT_API luaopen_cppunit(lua_State *L)
+{
+	luaL_register(L, "cppunit", cppunitLuaFunctions);
+	return 0;
+}
---
+++ testunit/test_runner.lua
@@ -0,0 +1,317 @@
+-- Global function
+-- ! They must be moved to some dependence package
+
+------------------------------------------------------
+local function isFunction(variable)
+------------------------------------------------------
+    return "function" == type(variable);
+end
+
+------------------------------------------------------
+local function isString(variable)
+------------------------------------------------------
+    return "string" == type(variable);
+end
+
+------------------------------------------------------
+local function isTable(variable)
+------------------------------------------------------
+    return "table" == type(variable);
+end
+
+
+local setmetatable, ipairs, tostring, pcall, require, dofile = setmetatable, ipairs, tostring, pcall, require, dofile;
+local debug_traceback = debug.traceback;
+local table, io, string, package, os = table, io, string, package, os;
+
+-- for debug
+local print = print;
+
+--------------------------------------------------------------------------------------------------------------
+module('testunit.test_runner');
+--------------------------------------------------------------------------------------------------------------
+
+function fakeFunction()
+end
+
+------------------------------------------------------
+TestListener = {};
+------------------------------------------------------
+
+function TestListener:new(o)
+    o = o or {};
+    setmetatable(o, self);
+    self.__index = self;
+    return o;
+end
+
+-- ??? Why succefsul or other test result doesn't indicate the end of tests
+function TestListener:addSuccessful(testCaseName)
+end
+
+function TestListener:addFailure(testCaseName, errorObject)
+end
+
+function TestListener:addError(testCaseName, errorObject)
+end
+
+function TestListener:addIgnore(testCaseName, errorObject)
+end
+
+function TestListener:startTest(testCaseName)
+end
+
+function TestListener:endTest(testCaseName)
+end
+
+function TestListener:startTests()
+end
+
+function TestListener:endTests()
+end
+
+function TestListener:outputMessage(message)
+end
+
+
+------------------------------------------------------
+TestObserver = {testListeners = {}};
+------------------------------------------------------
+
+function TestObserver:new(o)
+    o = o or {testListeners = {}};
+    setmetatable(o, self);
+    self.__index = self;
+    return o;
+end
+
+function TestObserver:addTestListener(listener)
+    self.testListeners[#self.testListeners + 1] = listener;
+end
+
+function TestObserver:callListenersFunction(functionName, ...)
+    for _, listener in ipairs(self.testListeners) do
+        listener[functionName](listener, ...);
+    end
+end
+
+function TestObserver:addSuccessful(testCaseName)
+    self:callListenersFunction("addSuccessful", testCaseName);
+end
+
+function TestObserver:addFailure(testCaseName, errorObject)
+    self:callListenersFunction("addFailure", testCaseName, errorObject);
+end
+
+function TestObserver:addError(testCaseName, errorObject)
+    self:callListenersFunction("addError", testCaseName, errorObject);
+end
+
+function TestObserver:addIgnore(testCaseName)
+    self:callListenersFunction("addIgnore", testCaseName);
+end
+
+function TestObserver:startTest(testCaseName)
+    self:callListenersFunction("startTest", testCaseName);
+end
+
+function TestObserver:endTest(testCaseName)
+    self:callListenersFunction("endTest", testCaseName);
+end
+
+function TestObserver:startTests()
+    self:callListenersFunction("startTests");
+end
+
+function TestObserver:endTests()
+    self:callListenersFunction("endTests");
+end
+
+------------------------------------------------------
+function isTestFunction(functionName)
+------------------------------------------------------
+    local exception = {"TEST_FIXTURE", "TEST_SUITE", "TEST_CASE", "TEST_CASE_EX", };
+    for _, v in ipairs(exception) do
+        if v == functionName then
+            return false;
+        end
+    end
+    functionName = string.lower(functionName);
+    if string.find(functionName, "^test") or string.find(functionName, "test$") then
+        return true;
+    end
+    return false;
+end
+
+------------------------------------------------------
+function runTestCase(testCaseName, testcase, testResult)
+------------------------------------------------------
+    local errorObjectDefault =
+    {
+        source = "";
+        func = "";
+        line = 0;
+        message = "";
+    };
+    local statusCode, errorObject = true, errorObjectDefault;
+
+    testResult:startTest(testCaseName);
+
+    if not testcase.isIgnored_ then
+        if testcase.setUp and isFunction(testcase.setUp) then
+            _, statusCode, errorObject = pcall(testcase.setUp, testcase);
+        else
+            statusCode, errorObject = true, errorObjectDefault;
+        end
+
+        if statusCode then
+            _, statusCode, errorObject = pcall(testcase.test, testcase);
+
+            if not statusCode then
+                testResult:addFailure(testCaseName, errorObject or errorObjectDefault);
+            else
+                testResult:addSuccessful(testCaseName);
+            end
+
+            if testcase.tearDown and isFunction(testcase.tearDown) then
+                _, statusCode, errorObject = pcall(testcase.tearDown, testcase);
+            else
+                statusCode, errorObject = true, errorObjectDefault;
+            end
+
+            if not statusCode then
+                testResult:addError(testCaseName, errorObject or errorObjectDefault);
+            end
+        else
+            testResult:addError(testCaseName, errorObject or errorObjectDefault);
+        end
+    else
+        testResult:addIgnore(testCaseName);
+    end
+
+    testResult:endTest(testCaseName);
+end
+
+------------------------------------------------------
+GlobalTestCaseList = {};
+
+TestCaseRecord = {};
+------------------------------------------------------
+
+function TestCaseRecord:new(name, object)
+    local o = {
+            ["name_"] = name,
+            ["test"] = object,
+        };
+    setmetatable(o, self);
+    self.__index = self;
+    return o;
+end
+
+function TestCaseRecord:name()
+    return self.name;
+end
+
+function TestCaseRecord:run(testResult)
+    runTestCase(self.name_, self.test, testResult);
+end
+
+function isWin()
+    local envVar = os.getenv('WINDIR') or os.getenv('HOME') or os.getenv('PWD');
+    return nil ~= string.match(envVar, '^%w:');
+end
+
+function isUnix()
+    local envVar = os.getenv('WINDIR') or os.getenv('HOME') or os.getenv('PWD');
+    return nil ~= string.match(envVar, '^/');
+end
+
+function loadLuaDriver(filePath)
+    dofile(filePath);
+end
+
+function loadCppDriver(filePath)
+    -- we must only load library to current process for initialization global objects and
+    -- filling test register
+    package.loadlib(filePath, "");
+end
+
+function isLuaTestDriver(filePath)
+    return nil ~= string.find(filePath, "%.t%.lua$")
+end
+
+function isCppTestDriver(filePath)
+    if isWin() then
+        return nil ~= string.find(filePath, "%.t%.dll$");
+    elseif isUnix() then
+        return nil ~= string.find(filePath, "%.t%.so$");
+    end
+end
+
+function initializeTestUnits()
+    if not package.loaded["testunit.lua_unit"] then
+        luaUnit = require("testunit.lua_unit");
+    end
+
+    if not package.loaded["cppunit"] then
+        if isWin() then
+            package.cpath = "../_bin/?.dll;"..package.cpath;
+        elseif isUnix() then
+            package.cpath = "../_bin/?.so;"..package.cpath;
+        end
+        cppUnit = require("cppunit");
+    end
+end
+
+function loadTestDrivers(filePathList)
+    initializeTestUnits();
+    local luaTestsArePresent, cppTestsArePresent = false, false;
+
+    for _, filePath in ipairs(filePathList) do
+        if isLuaTestDriver(filePath) then
+            loadLuaDriver(filePath);
+            luaTestsArePresent = true;
+        elseif isCppTestDriver(filePath) then
+            loadCppDriver(filePath);
+            cppTestsArePresent = true;
+        end
+    end
+
+    if luaTestsArePresent then
+        copyAllLuaTestCasesToGlobalTestList();
+    end
+
+    if cppTestsArePresent then
+        copyAllCppTestCasesToGlobalTestList();
+    end
+end
+
+------------------------------------------------------
+local function addTestCaseToGlobalList(name, object)
+    table.insert(GlobalTestCaseList, TestCaseRecord:new(name, object));
+end
+
+function copyAllLuaTestCasesToGlobalTestList()
+    local luaUnit = require("testunit.lua_unit");
+    local testcases = luaUnit.getTestList();
+    for _, testcase in ipairs(testcases) do
+        addTestCaseToGlobalList(testcase.name_, testcase);
+    end
+end
+
+function copyAllCppTestCasesToGlobalTestList()
+    local cppUnit = require("cppunit");
+    local testcases = cppUnit.getTestList();
+    for _, testcase in ipairs(testcases) do
+        addTestCaseToGlobalList(testcase.name_, testcase);
+    end
+end
+
+function runAllTestCases(testObserver)
+    testObserver = testObserver or TestObserver;
+    testObserver:startTests();
+    for _, testRecord in ipairs(GlobalTestCaseList) do
+        testRecord:run(testObserver);
+    end
+    testObserver:endTests();
+end
