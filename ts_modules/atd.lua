f727c561b847c3e4f4c96d9f163b4147c2d2dd31
teihrib <teihrib@localhost>
2010-12-24 17:57:58 +0500

    w04895 	?????? ??????????? atd ?? filesystem (r09634)

--- ts_modules/atd.lua
+++ ts_modules/atd.lua
@@ -397,7 +397,7 @@ local function convertTexTemplateIfNeeded(name)
         inform("File is LaTeX document, using template and macro. Begin parsing.");
         parseLineWithOpts(texFileOptions, line);
         -- add some options, with can't be founded from 1st line of TeX file
-        texFileOptions.dir = lfs.currentdir() .. '/';
+        texFileOptions.dir = lfs.currentdir() .. '\\';
         texFileOptions.macro['FILE_NAME'] = filename;
 
         expandTemplate(texFileOptions, name, intermediateFileExt)
