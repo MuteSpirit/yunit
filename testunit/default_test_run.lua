d84a59e655eab711245ac1762e2d0d7fb9405a89
teihrib <teihrib@localhost>
2011-02-01 10:10:07 +0500

    w05137 ??????? ??????? ??????? ?????? ?? ???????????

--- testunit/default_test_run.lua
+++ testunit/default_test_run.lua
@@ -2,6 +2,7 @@ local fs = require('filesystem')
 local testRunner = require('testunit.test_runner')
 local testListeners = require('testunit.test_listeners')
 
+-- test observer alone, because 'run' function may be called multiple times in one test run
 local testObserver = testRunner.TestObserver:new()
 testObserver:addTestListener(testListeners.TextTestProgressListener:new())
 
