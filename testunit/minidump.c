912ea17b631cae74a399306d054c0819f2003861
Efimov Ivan aka MuteSpirit <mutespirit@gmail.com>
2011-04-20 00:25:47 +0600

    add code to 'minidump' module draft

--- testunit/minidump.c
+++ testunit/minidump.c
@@ -26,20 +26,19 @@ extern "C" {
 #endif
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-struct CrashContext
+typedef struct _CrashContext
 {
     DWORD threadId_;            //!< id of thread, which has raised exception
     EXCEPTION_POINTERS* ep_;    //!< pointer for exception information structure
-};
-
-enum {cmdLineSize = 2 * MAX_PATH};
-static wchar_t cmdLine[cmdLineSize] = {0};
+} CrashContext;
 
 static void runCoroner(EXCEPTION_POINTERS* ep)
 {
+    enum {cmdLineSize = 2 * MAX_PATH};
+    wchar_t cmdLine[cmdLineSize] = {0};
     const wchar_t* coronerCmdLineFmtStr = L"lua5.1.exe -l minidump -e \"minidump.dumpProcess(%u, %u)\"";
 
-    static struct CrashContext crashContext = {0};
+    static CrashContext crashContext = {0};
     STARTUPINFOW si;
     PROCESS_INFORMATION pi;
 
@@ -72,6 +71,7 @@ static LONG __stdcall unhandledExceptionFilter(EXCEPTION_POINTERS* ep)
         Sleep(INFINITE);
 
     runCoroner(ep);
+    return 0;
 }
 
 static int setUnhandleExceptionHandler(lua_State* L)
@@ -84,8 +84,26 @@ static int setUnhandleExceptionHandler(lua_State* L)
 
 static int dumpProcess(lua_State* L)
 {
-    int argInd = 0, pid = 0, excContextAdr = 0;
-    
+    int pid = 0;
+    const void* excContextAdr = 0;
+    int argInd = 0;
+
+    unsigned long bytesRead = 0;
+
+    int useCrashContext = 0;
+
+    HANDLE hCrashProc = INVALID_HANDLE_VALUE;
+    HANDLE hDumpFile = INVALID_HANDLE_VALUE;
+    CrashContext crashContext;
+    wchar_t tempPath[MAX_PATH + 1];
+    wchar_t tempFileName[MAX_PATH + 1];
+    MINIDUMP_EXCEPTION_INFORMATION mei;
+    int minidumpType = MiniDumpWithDataSegs | 
+                       MiniDumpWithHandleData |
+                       MiniDumpWithUnloadedModules |
+                       MiniDumpWithProcessThreadData;
+    int rc = 0;
+
     argInd = 1;
     if (lua_isnumber(L, argInd))
     {
@@ -98,18 +116,96 @@ static int dumpProcess(lua_State* L)
     if (lua_isnumber(L, argInd))
     {
         lua_pushboolean(L, 0);
-        lua_pushfstring(L, "invalide 1st argument (integer expected, got %s)", lua_typename(L, lua_type(L, argInd)));
+        lua_pushfstring(L, "invalide 2nd argument (integer expected, got %s)", lua_typename(L, lua_type(L, argInd)));
         return 2;
     }
-    excContextAdr = lua_tointeger(L, 1);
+    excContextAdr = lua_topointer(L, 1);
 
+    hCrashProc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_TERMINATE | PROCESS_VM_READ, FALSE, pid);
+    if (INVALID_HANDLE_VALUE == hCrashProc)
+    {
+        lua_pushboolean(L, 0);
+        lua_pushfstring(L, "error opening process (pid = %u): %u\n", pid, GetLastError());
+		return 2;
+    }
+
+    if (0 == DebugActiveProcess(pid))
+    {
+        CloseHandle(hCrashProc);
+
+        lua_pushboolean(L, 0);
+        lua_pushfstring(L, "can't attach to process (pid = %u): %u\n", pid, GetLastError());
+		return 2;
+    }
+    //
+    // Read CrashContext from crashing process memory 
+    if (0 != ReadProcessMemory(hCrashProc, excContextAdr, &crashContext, sizeof(CrashContext), &bytesRead))
+        useCrashContext = 1;
+    //
+    // Create temporary file for dump writing
+    rc = GetTempPathW(MAX_PATH, tempPath);
+    if (0 == rc || rc > MAX_PATH)
+    {
+        CloseHandle(hCrashProc);
+
+        lua_pushboolean(L, 0);
+        lua_pushfstring(L, "GetTempPath failed: %u\n", GetLastError());
+		return 2;
+    }
+
+    if (0 == GetTempFileNameW(tempPath, L"_yunit", 0 /*create unique name*/, tempFileName))
+    {
+        CloseHandle(hCrashProc);
+
+        lua_pushboolean(L, 0);
+        lua_pushfstring(L, "GetTempFileNameW failed: %u\n", GetLastError());
+		return 2;
+    }
+
+    hDumpFile = CreateFileW(tempPath, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+    if (INVALID_HANDLE_VALUE == hDumpFile)
+    {
+        CloseHandle(hCrashProc);
+
+        lua_pushboolean(L, 0);
+        lua_pushfstring(L, "can't create file for dump failed: %u\n", GetLastError());
+		return 2;
+    }
+
+    if (useCrashContext)
+    {
+        mei.ThreadId          = crashContext.threadId_;
+        mei.ExceptionPointers = crashContext.ep_;
+	    mei.ClientPointers    = TRUE;
+    }
+
+    rc = MiniDumpWriteDump(hCrashProc, pid, hDumpFile, 
+        (MINIDUMP_TYPE)minidumpType, (useCrashContext) ? &mei : NULL, NULL, 0);
+
+    CloseHandle(hDumpFile);
+    CloseHandle(hCrashProc);
+
+    if (FALSE == rc)
+    {
+        lua_pushboolean(L, 0);
+        lua_pushfstring(L, "MiniDumpWriteDump failed: %u\n", GetLastError());
+		return 2;
+    }
+
+    lua_pushboolean(L, 1);
     return 1;
+}
+
+static int raiseException(lua_State* L)
+{
+    return (int)L / (int)NULL;
 }
 
 static const luaL_Reg minidumpLib[] = 
 {
     {"setCrashHandler", setUnhandleExceptionHandler},
     {"dumpProcess", dumpProcess},
+    {"raiseException", raiseException},
     {NULL, NULL}
 };
 
