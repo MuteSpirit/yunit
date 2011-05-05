20d211b1f63416f3cc2defaf3d72ccf5d8613ae0
Efimov Ivan <mutespirit@gmail.com>
2011-05-05 17:59:40 +0600

    Move class Thunk into test.* component. Decrease file count.

--- yunit/test.cpp
+++ yunit/test.cpp
@@ -27,6 +27,24 @@ const char** getTestContainerExtensions()
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Thunk::Thunk() throw()
+: thunkPtr_(0)
+, thisPtr_(0)
+{
+}
+
+Thunk::Thunk(void (* thunkPtr)(void*), void* thisPtr) throw()
+: thunkPtr_(thunkPtr)
+, thisPtr_(thisPtr)
+{
+}
+
+void Thunk::invoke()
+{
+    (*thunkPtr_)(thisPtr_);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class TestConditionException : public TestException
 {
 private:
--- yunit/test.h
+++ yunit/test.h
@@ -17,6 +17,23 @@
 #ifndef _YUNIT_TEST_HEADER_
 #define _YUNIT_TEST_HEADER_
 
+#ifndef YUNIT_API
+#	if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#		if defined(YUNIT_STATIC_LINKED)
+#			define YUNIT_API
+#		elif defined(YUNIT_DLL)
+#			define YUNIT_API __declspec(dllimport)
+#		else
+#			define YUNIT_API __declspec(dllexport)
+#		endif
+#	else
+#	   if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
+#			define YUNIT_API __attribute__ ((visibility("default")))
+#	   else
+#			define YUNIT_API
+#	   endif
+#	endif
+#endif
 
 #define YUNIT_NS yUnit
 
@@ -31,11 +48,31 @@
 
 #include <list>
 #include <string>
-#include "thunk.h"
 
 namespace YUNIT_NS {
 
 YUNIT_API const char** getTestContainerExtensions();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class YUNIT_API Thunk
+{
+public:
+    Thunk() throw();
+
+    template<typename T, void (T::* funcPtr)()>
+    static Thunk create(T* thisPtr) throw();
+
+    void invoke();
+
+private:
+    Thunk(void (* thunkPtr)(void*), void* thisPtr) throw();
+
+    template<typename T, void (T::* funcPtr)()>
+    static void thunk(void* thisPtr) throw();
+
+    void (* thunkPtr_)(void*);
+    void* thisPtr_;
+};
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class YUNIT_API SourceLine
@@ -205,6 +242,19 @@ private:
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Templates realization
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template<typename T, void (T::* funcPtr)()>
+Thunk Thunk::create(T* thisPtr) throw()
+{
+    return Thunk(&thunk<T, funcPtr>, thisPtr);
+}
+
+template<typename T, void (T::* funcPtr)()>
+void Thunk::thunk(void* thisPtr) throw()
+{
+    (static_cast<T*>(thisPtr)->*funcPtr)();
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 template<typename TestCaseClass>
