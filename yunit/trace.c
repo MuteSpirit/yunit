eb3a4ff8fff2b98b568218ade835072f5d6c3b14
Efimov Ivan <mutespirit@gmail.com>
2011-11-11 12:57:04 +0600

    fix (on Windows) moving trace Lua module to yunit.trace module

--- yunit/trace.c
+++ yunit/trace.c
@@ -1,29 +1,16 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// trace.cpp
+// trace.c
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#ifdef __cplusplus
-extern "C" {
-#endif
 #include <lua.h>
-#ifdef __cplusplus
-}
-#endif
+
+#define YUNIT_DLL_EXPORTS
+#include "yunit.h"
 
 #ifdef _WIN32
 #  include <windows.h>
 #else
 #  include <syslog.h>
-#endif
-
-#ifndef TRACE_API
-#	if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
-#		define TRACE_API __declspec(dllexport)
-#	elif defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
-#		define TRACE_API __attribute__ ((visibility("default")))
-#	else
-#		define TRACE_API
-#	endif
 #endif
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,7 +33,7 @@ int ltrace(lua_State* L)
 #ifdef __cplusplus
 extern "C"
 #endif
-int TRACE_API luaopen_trace(lua_State *L)
+int YUNIT_API luaopen_yunit_trace(lua_State *L)
 {
 	lua_register(L, "trace", ltrace);
 	return 0;
