bed6cd9af593dd08e3e8ca0e58b7b963fd189692
Efimov Ivan <mutespirit@gmail.com>
2011-11-19 18:37:03 +0500

    Issue 37 : finish yunit.trace.traceback function

--- yunit/trace.t.lua
+++ yunit/trace.t.lua
@@ -1,6 +1,20 @@
 local ytrace = require "yunit.trace"
 
-function ltraceback_use_case()
+function traceback_message()
+    local expectedMessage = "test error message"
+    local function step()
+        error(expectedMessage)
+    end
+    
+    local rc, traceback = xpcall(step, ytrace.traceback)
+    local stepInfo = debug.getinfo(step, "Sl")
+    
+    areEq(string.sub(stepInfo.source, 2), traceback.error.source)
+    areEq(stepInfo.linedefined + 1, traceback.error.line)
+    areEq(expectedMessage, traceback.error.message)
+end
+
+function traceback_stack_steps_use()
     local function step2()
         error("step2")
     end
@@ -8,26 +22,21 @@ function ltraceback_use_case()
         step2()
     end
 
-    local rc, stackTraceback = xpcall(step1, ytrace.traceback)
+    local rc, traceback = xpcall(step1, ytrace.traceback)
     
-    isTrue(string.find(stackTraceback.message, "trace%.t%.lua:5: step2"))
-    
-    for _, step in ipairs(stackTraceback.step) do
-        print(step.source .. ':' .. step.line .. ':' .. tostring(step.funcname))
-    end
-    isTrue(#stackTraceback.step > 2)
+    isTrue(#traceback.stack >= 3)
 
-    areEq("=[C]",             stackTraceback.step[1].source)
-    areEq(-1,                 stackTraceback.step[1].line)
-    areEq("function 'error'", stackTraceback.step[1].funcname)
+    areEq("=[C]",             traceback.stack[1].source)
+    areEq(-1,                 traceback.stack[1].line)
+    areEq("function 'error'", traceback.stack[1].funcname)
 
-    local step1info = debug.getinfo(step2, "Sl")
-    areEq(step1info.source,          stackTraceback.step[2].source)
-    areEq(step1info.linedefined + 1, stackTraceback.step[2].line)
-    areEq("function 'step2'",            stackTraceback.step[2].funcname)
+    local step2info = debug.getinfo(step2, "Sl")
+    areEq(step2info.source,          traceback.stack[2].source)
+    areEq(step2info.linedefined + 1, traceback.stack[2].line)
+    areEq("function 'step2'",         traceback.stack[2].funcname)
 
-    local step2info = debug.getinfo(step1, "Sl")
-    areEq(step2info.source,          stackTraceback.step[3].source)
-    areEq(step2info.linedefined + 1, stackTraceback.step[3].line)
-    isTrue(stackTraceback.step[3].funcname, step2info.source .. ":" .. step2info.linedefined))
+    local step1info = debug.getinfo(step1, "Sl")
+    areEq(step1info.source,             traceback.stack[3].source)
+    areEq(step1info.linedefined + 1,    traceback.stack[3].line)
+    isTrue(string.find(traceback.stack[3].funcname, ":" .. step1info.linedefined))
 end
