a8397cf7ea8cee74396ca8f039a420f171471f80
Efimov Ivan <mutespirit@gmail.com>
2012-02-04 20:42:09 +0600

    w0023 : Fix bug with stopping mine thread for Win32

--- yunit/mine.t.cpp
+++ yunit/mine.t.cpp
@@ -26,10 +26,11 @@ test(mine_usecase)
     MockDamageAgent terminator;
     Mine mine(&terminator);
 
-    mine.setTimer(Seconds(1));
+    mine.setTimer(Seconds(0));
     sleep(Seconds(1));
 
     isTrue(terminator.occured_);
+    mine.turnoff();
 }
 
 test(mine_neutralize)
