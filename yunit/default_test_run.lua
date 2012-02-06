abd6d29dbd4c83d202d65cc5073000b4dec6d6e2
Efimov Ivan <mutespirit@gmail.com>
2012-02-06 10:11:45 +0600

    w0022 : combine run and runFrom functions from default_test_run

--- yunit/default_test_run.lua
+++ yunit/default_test_run.lua
@@ -1,56 +1,31 @@
 local lfs = require "yunit.lfs"
+local fs = require "yunit.filesystem"
 local testRunner = require "yunit.test_runner"
 local testResultHandlers = require "yunit.test_result_handlers"
 
 local usedLtueArray = {}
 
---[=[ use this function to control what LTUE need to load and use, i.e. for usage only 'yunit.luaunit' run:
-lua -l yunit.work_in_scite -l yunit.default_test_run -e "use{'yunit.luaunit'}" -e "run[[test.t.lua]]"
-in command line
+--[=[ 
+    use this function to control what LTUE need to load and use, i.e. for usage only 'yunit.luaunit' run:
+    lua -l yunit.work_in_scite -l yunit.default_test_run -e "use{'yunit.luaunit'}" -e "run[[test.t.lua]]"
+    in command line
 --]=]
 function use(ltueArray)
     usedLtueArray = ltueArray
 end
 
-local makeDefaultTestRunner
-
-function runFrom(dirPaths)
-    local runner = makeDefaultTestRunner()
-    local fixFailed = testResultHandlers.FixFailed:new()
-    runner:addResultHandler(fixFailed)
-    runner:addLoadtHandler(fixFailed)
-
-    for _, dirPath in pairs(dirPaths) do
-        runner:lookTestsAt(dirPath)
+--[=[ 
+    @param[in] inArg Maybe: 
+                     1) path to test container
+                     2) path of directory with test containers (recursive search)
+                     3) table with elements from item 1, 2, 3
+--]=]
+function run(inArg)
+    if nil == inArg then
+        error('not nill expected as argument, but was one')
+        return
     end
-    
-    runner:runAll()
 
-    if not fixFailed:passed() then
-        print(fixFailed:message())
-        io.stdout:flush()
-        io.stderr:flush()
-        os.exit(-1)
-    end
-end
-
-function run(testContainerPath)
-    local runner = makeDefaultTestRunner()
-    local fixFailed = testResultHandlers.FixFailed:new()
-    runner:addResultHandler(fixFailed)
-    runner:addLoadtHandler(fixFailed)
-    
-    runner:runTestsOf(testContainerPath)
-
-    if not fixFailed:passed() then
-        print(fixFailed:message())
-        io.stdout:flush()
-        io.stderr:flush()
-        os.exit(-1)
-    end
-end
-
-makeDefaultTestRunner = function()
     local runner = testRunner.TestRunner:new()
 
     runner:addResultHandler(testResultHandlers.EstimatedTime:new())
@@ -71,6 +46,42 @@ makeDefaultTestRunner = function()
             runner:loadLtue(ltueName)
         end
     end
-    
-    return runner
+
+    local fixFailed = testResultHandlers.FixFailed:new()
+    runner:addResultHandler(fixFailed)
+    runner:addLoadtHandler(fixFailed)
+
+    local function handleArg(arg)
+        if 'string' == type(arg) then
+            local path = arg
+
+            if not fs.isExist(path) then
+                error('receive path to unexist file/directory')
+                return
+            elseif fs.isFile(path) then
+                runner:runTestContainer(path)
+            elseif fs.isDir(path) then
+                runner:runTestContainersFromDir(path)
+            else
+                error('receive path to unknown file system object type (not file and not directory)')
+                return
+            end
+        elseif 'table' == type(arg) then
+            for _, path in pairs(arg) do
+                handleArg(path)
+            end
+        else
+            error('table or string expected, but was %s', type(arg))
+            return
+        end
+    end
+
+    handleArg(inArg)
+
+    if not fixFailed:passed() then
+        print(fixFailed:message())
+        io.stdout:flush()
+        io.stderr:flush()
+        os.exit(-1)
+    end
 end
