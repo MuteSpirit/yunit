507a1bba8d1393ce20c4309d280efa2914467188
Ivan Efimov <MuteSpirit@gmail.com>
2012-02-12 00:06:22 +0600

    w0017 : add detection NetBeans as parent process

--- yunit/aux.cpp
+++ yunit/aux.cpp
@@ -28,22 +28,12 @@ int YUNIT_API luaopen_yunit_aux(lua_State* L)
     return 1;
 }
 
-LUA_META_METHOD(Aux, pid)
-{
-    Lua::State lua(L);
-#if defined(_WIN32)
-    lua.push(::GetCurrentProcessId());
-#else
-    lua.push(getpid());
-#endif
-    return 1;
-}
 
+#if defined(_WIN32)
 LUA_META_METHOD(Aux, allProccesses)
 {
     Lua::State lua(L);
 
-#if defined(_WIN32)
     const DWORD currentProcessFlag = 0;
     HANDLE procSnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, currentProcessFlag);
     if (INVALID_HANDLE_VALUE == procSnapshot)
@@ -81,121 +71,97 @@ LUA_META_METHOD(Aux, allProccesses)
     while(::Process32Next(procSnapshot, &procInfo));
 
     ::CloseHandle(procSnapshot);
+    return 1;
+}
+
+#else // defined(_WIN32)
+
+LUA_META_METHOD(Aux, pid)
+{
+    Lua::State lua(L);
+#if defined(_WIN32)
+    lua.push(::GetCurrentProcessId());
 #else
+    lua.push(getpid());
+#endif
+    return 1;
+}
+
+LUA_META_METHOD(Aux, exePath)
+{
+    Lua::State lua(L);
+    
+    enum Args {pidIdx = 1};
 
 #define MAXLINE 8192 // got from pstree.p
     enum {pathSize = MAXLINE};
     char path[pathSize];
 
-    DIR *procDir;
-    struct dirent *dirEnt;
-    const char* procPath = "/proc/";
+    int cChars = snprintf(path, pathSize - 1, "/proc/%d/exe", lua_tointeger(L, pidIdx));
+    path[cChars] = '\0';
 
-    procDir = opendir(procPath);
-    if (NULL == procDir)
-        return lua.error("cannot open '%s' directory", procPath);
-
-    struct stat dirStat;
-    lua.push(Lua::Table());
-    const int procsTableIdx = lua.top();
-    int dirEntIdx;
-    
-    while (dirEnt = readdir(procDir))
+    errno = 0;
+    ssize_t cBytes = readlink(path, path, pathSize - 1);
+    if (-1 == cBytes)
     {
-        lstat(dirEnt->d_name, &dirStat);
-        if (!S_ISDIR(dirStat.st_mode))
-            continue;
-        
-        if (strcmp(".", dirEnt->d_name) == 0 || strcmp("..", dirEnt->d_name) == 0)
-            continue;
-        
-        // detect that dirname is pid number
-        bool isNotPid = false;
-        for (const char* p = dirEnt->d_name; p && *p; ++p)
-        {
-            if (!isdigit(*p))
-            {
-                isNotPid = true;
-                break;
-            }
-        }
-        
-        if (isNotPid)
-            continue;
-        
-        lua.push(Lua::Table());     /* stack: procInfo */
-        dirEntIdx = lua.top();
+        return lua.error("readlink('%s') failed, errno = %d, EACCES = %d\n", path, errno, EACCES);
+    }
+    else
+    {
+        lua.push(path, cBytes);
+        return 1;
+    }
+}
 
-        int cChars = snprintf(path, pathSize - 1, "/proc/%s/exe", dirEnt->d_name);
-        path[cChars] = '\0';
-        
-//        errno = 0;
-//        ssize_t cBytes = readlink(path, path, pathSize - 1);
-//        if (-1 == cBytes)
-//        {
-//            printf("readlink('%s') failed, errno = %d, EACCES = %d\n", path, errno, EACCES);
-//            lua.remove(dirEntIdx);
-//            continue;
-//        }
-//        else
-//        {
-//            lua.push(path, cBytes);
-//            lua.setfield(dirEntIdx, "exe");
-//        }
-//
-        cChars = snprintf(path, pathSize - 1, "/proc/%s/status", dirEnt->d_name);
-        path[cChars] = '\0';
-        
-        FILE* f = fopen(path, "r");
-        if (NULL == f)
+LUA_META_METHOD(Aux, ppid)
+{
+    Lua::State lua(L);
+    
+    enum Args {pidIdx = 1};
+
+#define MAXLINE 8192 // got from pstree.p
+    enum {pathSize = MAXLINE};
+    char path[pathSize];
+
+    int cChars = snprintf(path, pathSize - 1, "/proc/%d/status", lua_tointeger(L, pidIdx));
+    path[cChars] = '\0';
+
+    FILE* f = fopen(path, "r");
+    if (NULL == f)
+        return lua.error("Cannot open '%s' to detect parent proccess pid", path);
+
+    const char* ppidPrefix = "PPid:";
+    enum {ppidPrefixSize = sizeof("PPid:") - sizeof(char)};
+
+    while (fgets(path, pathSize, f) != NULL) /* until reach EOF */
+    {
+        const char* p = path;
+        for (; *p && !isalpha(*p); ++p)
+            ;
+                
+        if (0 == strncmp(ppidPrefix, p, ppidPrefixSize))
         {
-            closedir(procDir);
-            return lua.error("Cannot open '%s' to detect parent proccess pid", path);
-        }
-        
-        const char* ppidPrefix = "Ppid:";
-        enum {ppidPrefixSize = sizeof("Ppid:") - sizeof(char)};
-        
-        while (fgets(path, pathSize, f) != NULL) /* until reach EOF? */
-        {
-            if (0 == strncmp(ppidPrefix, path, ppidPrefixSize))
+            unsigned long ppid = 0;
+            
+            for (p += ppidPrefixSize; *p && !isdigit(*p); ++p)
+                ;
+            
+            for (; *p && isdigit(*p); ++p)
             {
-                unsigned long ppid = 0;
-                char c;
-                for (const char* p = path + ppidPrefixSize; p && *p; ++p)
-                {
-                    c = *p;
-                    if (' ' == c)
-                        continue;
-                    else if (!isdigit(c))
-                        break;
-                    else
-                    {
-                        ppid *= 10;
-                        ppid += c - '0';
-                    }
-                }
-                
-                lua.push(ppid);
-                lua.setfield(dirEntIdx, "ppid");
-                break;
+                ppid *= 10;
+                ppid += *p - '0';
             }
+
+            lua.push(ppid);
+            fclose(f);
+            return 1;
         }
-                
-        fclose(f);
-        
-        lua.push(atoi(dirEnt->d_name));
-        lua.push(Lua::Value(dirEntIdx));
-        lua.settable(procsTableIdx);
-        
-        lua.remove(dirEntIdx);
     }
 
-    closedir(procDir);
-            
+    fclose(f);
+    return 0;
+}
 
 #endif // defined(_WIN32)
-    return 1;
-}
 
 } // namespace YUNIT_NS
