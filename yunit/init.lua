795246cc0811e6a1c9aa44a726bb7978cb71cc0a
Ivan Efimov <MuteSpirit@gmail.com>
2012-02-12 00:06:22 +0600

    w0017 : add detection NetBeans as parent process

--- yunit/init.lua
+++ yunit/init.lua
@@ -2,42 +2,60 @@
 require "yunit.default_test_run"
 
 local aux = require "yunit.aux"
+local fs = require "yunit.filesystem"
 
-require "yunit.work_in_netbeans"
+if 'win' == fs.whatOs() then
+    local curProcParentPid = proccesses[aux.pid()].ppid 
 
---[[
-local proccesses = aux.allProccesses()
-print(proccesses, #proccesses)
+    local proc = proccesses[curProcParentPid]
+    while proc 
+    do
+        local exe = string.lower(proc.exe)
+        if string.find(exe, 'devenv.exe') then
+            require "yunit.work_in_vs"
+            break;
+        elseif string.find(exe, 'netbeans') then
+            require "yunit.work_in_netbeans"
+            break;
+        elseif string.find(exe, 'scite') then
+            require "yunit.work_in_scite"
+            break;
+        elseif string.find(exe, 'cmd.exe') then
+            require "yunit.work_in_cmd"
+            break;
+        elseif string.find(exe, 'bash') then
+            require "yunit.work_in_bash"
+            break;
+        end
 
-local curProcParentPid = proccesses[aux.pid()].ppid 
-print(aux.pid(), proccesses[aux.pid()], proccesses[aux.pid()]['ppid'])
+        proc = proccesses[proc.ppid]
+    end
+else
+    local pid = aux.pid()
+    local ppid = aux.ppid(pid)
+    local reachedInitProcess = nil == ppid or 1 == ppid
+    
+    while not reachedInitProcess
+    do
+        local exe = string.lower(aux.exePath(pid))
+        if string.find(exe, 'devenv.exe') then
+            require "yunit.work_in_vs"
+            break;
+        elseif string.find(exe, 'netbeans') then
+            require "yunit.work_in_netbeans"
+            break;
+        elseif string.find(exe, 'scite') then
+            require "yunit.work_in_scite"
+            break;
+        end
 
-local proccesses = aux.allProccesses()
-print(proccesses, #proccesses)
-
-local curProcParentPid = proccesses[aux.pid()].ppid 
-
-local proc = proccesses[curProcParentPid]
-while proc 
-do
-    local exe = string.lower(proc.exe)
-    if string.find(exe, 'devenv.exe') then
-        require "yunit.work_in_vs"
-        break;
-    elseif string.find(exe, 'netbeans') then
-        require "yunit.work_in_netbeans"
-        break;
-    elseif string.find(exe, 'scite') then
-        require "yunit.work_in_scite"
-        break;
-    elseif string.find(exe, 'cmd.exe') then
-        require "yunit.work_in_cmd"
-        break;
-    elseif string.find(exe, 'bash') then
-        require "yunit.work_in_bash"
-        break;
+        pid = ppid
+        ppid = aux.ppid(pid)
+        reachedInitProcess = nil == ppid or 1 == ppid
     end
 
-    proc = proccesses[proc.ppid]
+    if reachedInitProcess then
+        require "yunit.work_in_scite"
+    end
 end
---]]
+
