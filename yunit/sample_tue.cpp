d33b7df91069cd7a9d4a16d6cfaf4f4430b8c4b6
Ivan Efimov <MuteSpirit@gmail.com>
2012-08-21 23:51:26 +0600

    refactoring new test unit engine prototype

--- yunit/sample_tue.cpp
+++ yunit/sample_tue.cpp
@@ -2,52 +2,60 @@
 #include <stdio.h>
 #include <string.h>
 
-struct StubTest
+template<typename T, void (T::*method)(LoggerPtr)>
+void callAdapter(void *t, LoggerPtr logger)
 {
+    (static_cast<T*>(t)->*method)(logger);
+}
+
+struct StubTest : public Test
+{
+    typedef StubTest Self;
+    StubTest()
+    {
+        self_ = this;
+        setUp_ = &callAdapter<Self, &Self::setUp>;
+        test_ = &callAdapter<Self, &Self::test>;
+        tearDown_ = &callAdapter<Self, &Self::tearDown>;
+        name_ = &Self::name;
+    }
+    
     void setUp(LoggerPtr logger)
     {
-        printf("StubTest::setUp\n");
+        logger->success_(logger->self_);
     };
     
     void test(LoggerPtr logger)
     {
-        printf("StubTest::test\n");
-        logger->success_(logger->self_, NULL);
+        logger->success_(logger->self_);
     };
     
     void tearDown(LoggerPtr logger)
     {
-        printf("StubTest::tearDown\n");
+        logger->success_(logger->self_);
     };
+    
+    const char* name() const
+    {
+        return "StubTest";
+    }
+
+    static const char* name(const void *self)
+    {
+        return ((const StubTest*)self)->name();
+    }
 };
-
-void setUpStubTest(void *self, LoggerPtr logger)
-{
-    ((StubTest*)self)->setUp(logger);
-}
-
-void testStubTest(void *self, LoggerPtr logger)
-{
-    ((StubTest*)self)->test(logger);
-}
 
 Test** loadTestContainer(const char *path)
 {
-    static StubTest stubTest;
-    static Test test;
+    static StubTest test;
     static Test* tests[] = {&test, NULL};
 
-    memset(&test, 0, sizeof(test));
-    test.self_ = &stubTest;
-    test.setUp_ = &setUpStubTest;
-    test.test_ = &testStubTest;
-    
     return tests;
 }
 
-static const char* supportedExts[] = {"t.so", NULL};
-
 const char** testContainerExtensions()
 {
+    static const char* supportedExts[] = {"t.so", NULL};
     return supportedExts;
 }
