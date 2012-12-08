74c9fc646938e6ef36ee1ac76580a2f3f1a720f6
Ivan Efimov <mutespirit@gmail.com>
2012-12-08 19:47:04 +0600

    refactoring test engine and TestContainer interfaces

--- yunit/test_engine_interface.h
+++ yunit/test_engine_interface.h
@@ -8,8 +8,13 @@
 /// 3) define all functions from current file, marked with attribute TUE_API (they will be exported, 
 /// 4) build library as DLL
 /// 
-/// @todo Think about strategy of TestCase, TestError creation and destroing
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @todo Think about how TestCase::test* methods say about asserts passes, pass messages
+/// @todo Try to pass TestError objects into setUp, testBody, tearDown
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @todo @done Think about strategy of TestCase, TestError creation and destroing
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #ifndef _TEST_ENGINE_INTERFACE_HEADER_
 #define _TEST_ENGINE_INTERFACE_HEADER_
 
@@ -38,17 +43,20 @@ extern "C" {
 #endif // ifndef TUE_API
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// API functions:
+// Test Engine API functions
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct _TestContainer;
 typedef struct _TestContainer TestContainer, *TestContainerPtr;
 
-/// @brief Create new TestContainer object
+/// @brief Fill TestContainer object
 /// @param[in] path Path to test container file (usually, but really it maybe path to some other resource)
-TUE_API TestContainerPtr newTestContainer(const char *path);
+/// So test engine library must not create struct TestContainer object, but only create it's TestContainer 
+/// implementation object.
+TUE_API void loadTestContainer(TestContainerPtr tcPtr, const char *path);
 
-/// @brief Delete TestContainer object
-TUE_API void closeTestContainer(TestContainerPtr tcPtr);
+/// @brief Clear TestContainer object and say library that it may destroy it's object of TestContainer
+/// implementation.
+TUE_API void unloadTestContainer(TestContainerPtr tcPtr);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 struct _TestCase;
@@ -56,18 +64,29 @@ typedef struct _TestCase TestCase, *TestCasePtr;
 
 struct _TestContainer
 {
+    /// @brief Pointer to real object, created inside test engine
     void *self_;
 
-    /// @brief load test container file and tests from it
-    /// @return list of TestCase objects or NULL in case of tests abscent
-    TestCasePtr (*load_)(void *self); 
+    /// @param[in] self Pass 'self_'
+    /// @return Number of tests, contained at test container
+    ///
+    /// It allow create required number of TestCase objects to execute 'load_' method
+    unsigned int (*numberOfTests_)(void *self);
 
-    /// unload test container file. You must not use got TestCase objects after this method call
-    /// @param[in] self "this" pointer
+    /// @brief load test container file and tests from it
+    /// @param[in] self Pass 'self_'
+    /// @param[in, out] testList Client code must create list with such many TestCase objects, as 'numberOfTests_'
+    ///                          method sad
+    void (*load_)(void *self, TestCasePtr testList); 
+
+    /// @brief unload test container file. Free TestCase's members, besides of 'next_'
+    /// @param[in] self Pass 'self_'
     /// @param[in] testList list of tests, returned from load_ call
     bool (*unload_)(void *self, TestCasePtr testList); 
 
-    /// @return Last occured error's message. String must be copied by client code before usage.
+    /// @param[in] self Pass 'self_'
+    /// @return Last occured error's message. Client code must use only copy of this string.
+    ///         Test engine library may delete it in any next call.
     const char* (*errMsg_)(void *self); 
 };
 
@@ -79,33 +98,47 @@ typedef struct _TestError TestError;
 /// @details You should return test, even it must be ignored, because information hiding is wrong strategy
 struct _TestCase
 {
-    /// real obect "this" pointer
+    /// @brief Pointer to real object, created inside test engine
     void *self_;
     
+    /// @param[in] self Pass 'self_'
     /// will be called before 'testBody'
     /// @return true in case of succes and false otherwise
     bool (*setUp_)(void *self);
 
+    /// @param[in] self Pass 'self_'
     /// test's 'main' function, will be called if 'setUp' has been success
     /// @return true in case of succes and false otherwise
     bool (*testBody_)(void *self);               
 
+    /// @param[in] self Pass 'self_'
     /// will be called, if 'setUp' has been success
     /// @return true in case of succes and false otherwise
-    bool (*tearDown_)(void *self);           
+    bool (*tearDown_)(void *self); 
 
+    /// @param[in] self Pass 'self_'
     /// will be called if any of 'setUp', 'testBody' or 'tearDown' functions return false to get 
     /// @param[out] errorInfo it will be fiiled with information about last error
     void (*error_)(void *self, TestError *errorInfo); 
 
+    /// @param[in] self Pass 'self_'
     /// @return 1 if test must be ignored and not executed and 0 otherwise
     int (*ignored_)(const void *self);
 
-    const char* (*name_)(const void *self);  ///< @return test name
-    const char* (*source_)(const void *self);///< @return full path of file, containing test source
-    int (*line_)(const void *self);          ///< @return number of 1st line of test definition
-    
-    struct _TestCase* next_;                    ///< pointer to next Test in list
+    /// @param[in] self Pass 'self_'
+    /// @return test name
+    const char* (*name_)(const void *self);
+
+    /// @param[in] self Pass 'self_'
+    /// @return full path of file, containing test source
+    const char* (*source_)(const void *self);
+
+    /// @param[in] self Pass 'self_'
+    /// @return number of 1st line of test definition
+    int (*line_)(const void *self);
+
+    /// pointer to next list element
+    struct _TestCase* next_;
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -115,21 +148,30 @@ struct _TestCase
 // They are defined here, because it is not reasonable to force test unit engine authors to define them 
 // themselves.
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-inline const char* testContainerErrMsg(TestContainerPtr tc)
+// TestContainer
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+inline unsigned int testContainerNumberOfTests(TestContainerPtr tc)
 {
-    return tc->errMsg_(tc->self_);
+    return tc->numberOfTests_(tc->self_);
 }
 
-inline TestCasePtr testContainerLoad(TestContainerPtr tc)
+inline void testContainerLoad(TestContainerPtr tc, TestCasePtr testList) 
 {
     return tc->load_(tc->self_);
 }
 
-inline bool testContainerUnload(TestContainerPtr tc, TestCasePtr testList)
+inline bool testContainerUnload(TestContainerPtr tc, TestCasePtr testList) 
 {
-    return tc->unload_(tc->self_, testList);
+    return tc->unload_(tc->self_);
 }
 
+inline const char* testContainerErrMsg(TestContainerPtr tc) 
+{
+    return tc->errMsg_(tc->self_);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// TestCase
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 inline bool setUp(TestCasePtr test)
 {
